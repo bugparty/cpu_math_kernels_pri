767544cb03855e2b6ea27ee5ef6660c5e5018053
bowen han <bhan001@ucr.edu>
2024-10-14 15:08:43 -0700

    q2 finished

--- hw1/bench2.py
+++ hw1/bench2.py
@@ -2,7 +2,6 @@ import os
 os.system("make tester > /dev/null")
 os.system('echo "func, size, padding,time, correctness"')
 for i in range(6,12):
-   os.system("srun tester dgemm0 "+str(2**i)+" 1")
-   os.system("srun tester dgemm1 "+str(2**i)+" 1")
+   os.system("srun tester dgemm2 "+str(2**i)+" 1")
    
 
